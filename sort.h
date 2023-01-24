d2f3faf78c275f3c120d2b6da6862b18b79f8f00
myra-suk <112114209+myra-suk@users.noreply.github.com>
2023-01-24 23:52:34 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -1,15 +1,15 @@
-#ifndef _SORT_
-#define _SORT_
-
-#include<stdlib.h>
+#ifndef SORT_HEADER
+#define SORT_HEADER
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
- *
- * @n: Integer
- * @next: Pointer to the next element of the list.
- * @prev: Pointer to the previous element of the list.
+ * @n: Integer stored in the node
+ * @next: Pointer to the next node of the list
+ * @prev: Pointer to the previous node of the list
  */
+ 
 typedef struct listint_s
 {
 	const int n;
@@ -17,22 +17,14 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+void swap(int *arr, int idx1, int idx2);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+listint_t *create_listint(const int *array, size_t size);
+void swap_list(listint_t *pn, listint_t *temp, listint_t *nxt, listint_t **h);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void shell_sort(int *array, size_t size);
-void counting_sort(int *array, size_t size);
-void radix_sort(int *array, size_t size);
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
-void merge_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void _ssort(int *a, int size, int n);
-void _qsort(int *a, int low, int high, int size);
-void cocktail_sort_list(listint_t **list);
-void recursion_heap(int *arr, int i, size_t size, int limit);
-void swap_nums(int *arr, int a, int b);
-
 
 #endif
