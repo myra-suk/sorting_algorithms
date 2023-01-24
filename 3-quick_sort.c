4f5f6a78c6849bdf3193dadcc76e2266f8c1ece5
myra-suk <112114209+myra-suk@users.noreply.github.com>
2023-01-25 01:23:47 +0300

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,95 +1,104 @@
-include "sort.h"
+#include "sort.h"
 
-void divide(int beg, int pivot, int *i, size_t size);
-int partition(int beg, int pivot, int *i, size_t size);
-void swap_int(int *a, int *b);
 /**
- * quick_sort - sorts an array of integers in ascending order
- * @array: array to be sorted
- * @size: size of the array
+ * quick_sort - Sorts an array of integers using quick sort algorithm
+ * @array: The array to sort
+ * @size: The size of the array
+ *
+ * Return: Nothing
  */
+
 void quick_sort(int *array, size_t size)
 {
-	int beg = 0, pivot;
+	size_t pivot;
 
-	if (array && size > 1)
-	{
-		pivot = (size - 1);
-		divide(beg, pivot, array, size);
-	}
-}
-/**
-* divide - recursively partition
-* @beg: beginning of divided array
-* @pivot: end of divided array
-* @i: the beginning of the array
-* @size: size of array
-**/
-void divide(int beg, int pivot, int *i, size_t size)
-{
-	int first, second, np;
+	if (!array || size < 2)
+		return;
 
-	if (beg < pivot)
-	{
-		second = partition(beg, pivot, i, size);
-		first = beg;
-		np = second - 1;
-		if (first != np && second != pivot)
-			np--;
-		divide(first, np, i, size);
-		divide(second, pivot, i, size);
-	}
-}
-/**
-* partition - divides an array
-* @beg: beginning of array separated
-* @pivot: end of array separated
-* @i: the beginning of array
-* @size: size of array
-* Return: the new beginning
-**/
-int partition(int beg, int pivot, int *i, size_t size)
-{
-	int temp;
+	print_sort(array, size, 1);
 
-	temp = beg;
-	while (temp != pivot)
-	{
-		if (i[temp] < i[pivot])
-		{
-			if (temp != beg)
-			{
-				swap_int(i + temp, i + beg);
-				print_array(i, size);
-			}
-			temp++;
-			beg++;
-		}
-		else
-			temp++;
-	}
-	if (beg != pivot)
-	{
-		if (i[beg] > i[pivot])
-		{
-			swap_int(i + pivot, i + beg);
-			print_array(i, size);
-		}
-		beg++;
-	}
-	return (beg);
+	pivot = partition(array, size);
+
+	quick_sort(array, pivot);
+
+	quick_sort(array + pivot, size - pivot);
+
 }
 
 /**
-  * swap_int - swaps the values of two integers
-  * @a: take an int
-  * @b: take an int
-  */
-void swap_int(int *a, int *b)
+ * swap - Swaps two values
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Return: Nothing
+ */
+
+void swap(int *a, int *b)
 {
 	int temp;
 
 	temp = *a;
 	*a = *b;
 	*b = temp;
+}
+
+/**
+ * partition - Sets the pivot for quick sort
+ * @array: The array to partition
+ * @size: The size of the array
+ *
+ * Return: (i+1)
+ */
+
+size_t partition(int array[], size_t size)
+{
+	int pivot;
+	size_t i = -1;
+	size_t j;
+
+	if (!array || size < 2)
+		return (0);
+
+	pivot = array[size - 1];
+
+	for (j = 0; j < size - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_sort(array, size, 0);
+			}
+		}
+	}
+	if (i + 1 != size - 1)
+	{
+		swap(&array[i + 1], &array[size - 1]);
+		print_sort(array, size, 0);
+	}
+	return (i + 1);
+}
+
+/**
+ * print_sort - Function that prints as it should
+ * @array: Array to be printed
+ * @size: Size of array
+ * @init: Should initialize array
+ * Return: 0
+ */
+
+void print_sort(int array[], size_t size, int init)
+{
+	static int *p = (void *)0;
+	static size_t s;
+
+	if (!p && init)
+	{
+		p = array;
+		s = size;
+	}
+	if (!init)
+		print_array(p, s);
 }
